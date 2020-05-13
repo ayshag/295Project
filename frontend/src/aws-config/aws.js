9bb917d5cb7001071c63236d8eb6739171ade791
ayshag <43154634+ayshag@users.noreply.github.com>
2020-05-13 12:26:36 -0700

    Web server/client code

--- frontend/src/aws-config/aws.js
+++ frontend/src/aws-config/aws.js
@@ -2,7 +2,7 @@ var AWS = require('aws-sdk');
 
 AWS.config = new AWS.Config();
 
-AWS.config.accessKeyId = "AKIAXBXQVKC4TY2ZUR6V";
-AWS.config.secretAccessKey = "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";
+AWS.config.accessKeyId = "AWSAccessKeyId";
+AWS.config.secretAccessKey = "AWSSecretAccessKey";
 AWS.config.region = "us-east-1";
 module.exports = AWS;
